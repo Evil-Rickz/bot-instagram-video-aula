4cc66114c1d2baa542af9625269f80d1d3c4505f
Evil-Rickz <94098966+Evil-Rickz@users.noreply.github.com>
2022-03-15 13:53:10 -0300

    Update follow.js

--- follow.js
+++ follow.js
@@ -1,4 +1,3 @@
-// Seguir automaticamente
 let contador = 0;
 
 document.querySelectorAll('.sqdOP.L3NKy.y3zKF').forEach((item, index) => {
@@ -6,7 +5,7 @@ document.querySelectorAll('.sqdOP.L3NKy.y3zKF').forEach((item, index) => {
 		if(!item.classList.contains('._8A5w5')){
         	item.click();
         	contador++;
-			console.log(`Seu BOT já seguiu ${contador} pessoa(s)!`);
+			console.log(`Seu BOT já seguiu ${contador} pessoa(s)`);
 		} else { 
 			console.log('Você já segue essa pessoa.');
     }
