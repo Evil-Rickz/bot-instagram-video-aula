8a11fb1842ad6b644236e4a3a3e9645de1a6ab85
Rick <94098966+Evil-Rickz@users.noreply.github.com>
2022-11-30 12:07:53 -0300

    Update unfollow.js

--- unfollow.js
+++ unfollow.js
@@ -1,15 +1,18 @@
 // Deixar de seguir automaticamente
 let contador = 0;
-document.querySelectorAll('._8A5w5').forEach((item, index) => {
+document.querySelectorAll('._acat').forEach((item, index) => {
     setTimeout(() => {
         if(item.innerText == 'Seguindo') {
             item.click();
-            document.querySelectorAll('.-Cab_').forEach(item => {
+            document.querySelectorAll('._a9-_').forEach(item => {
                 if(item.innerText == 'Deixar de seguir') {
                     item.click();
                     contador++;
                     console.log(`Você deixou de seguir ${contador} pessoa(s)!`)
                 }
+            document.querySelectorAll('._a9_1').forEach(item => {
+                if(item.innerText == 'OK') {
+                    item.click();
             })
         } 
     }, index * 10000);
